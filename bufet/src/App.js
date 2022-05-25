263685934d6a8b74b76046050c2b49f13b40f1f3
EldanizQa <kewa.miki2@gmail.com>
2022-05-26 02:28:29 +0400

    Map example added by Eldaniz

--- bufet/src/App.js
+++ bufet/src/App.js
@@ -1,9 +1,25 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <MapContainer center={[40.36758192810607, 49.93856174974282]} zoom={8} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    {/* <Marker position={[40.36758192810607, 49.93856174974282]}>
+      <Popup>
+        Qasimov Huseyn Abdulla oglu <br /> Hal Hazirda dashag yaliyir.
+      </Popup>
+    </Marker> */}
+    <Marker position={[40.36491294861612, 47.73238271138779]}>
+      <Popup>
+        <a href='https://en.wikipedia.org/wiki/Azerbaijan'>Welcome</a> <br /> 
+      </Popup>
+    </Marker>
+  </MapContainer>
   );
 }
 
