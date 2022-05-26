1fc47202a9dc43519d71455ef3a272650acbe332
EldanizQa <kewa.miki2@gmail.com>
2022-05-26 14:28:05 +0400

    Delete trash

--- bufet/src/App.js
+++ bufet/src/App.js
@@ -9,11 +9,7 @@ function App() {
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    {/* <Marker position={[40.36758192810607, 49.93856174974282]}>
-      <Popup>
-        Qasimov Huseyn Abdulla oglu <br /> Hal Hazirda dashag yaliyir.
-      </Popup>
-    </Marker> */}
+    
     <Marker position={[40.36491294861612, 47.73238271138779]}>
       <Popup>
         <a href='https://en.wikipedia.org/wiki/Azerbaijan'>Welcome</a> <br /> 
