aebd57435b143fef45c9a975ff8f8c88920eb680
Macbook <macbook2020@Macbooks-MacBook-Pro.local>
2022-05-26 13:46:00 +0400

    navbar

--- bufet/src/App.js
+++ bufet/src/App.js
@@ -1,8 +1,19 @@
-import './App.css';
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import MainPage from "./pages/MainPage/MainPage";
+import ListPage from "./pages/ListPage/ListPage";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" exact component={MainPage} />
+        <Route path="/list/:id" exact component={ListPage} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
