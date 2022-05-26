82c5954c9a194cb847251e5dfadbe0c2c70dde18
EldanizQa <kewa.miki2@gmail.com>
2022-05-27 00:20:45 +0400

    Made interactive map and geoJson

---
+++ bufet/src/App1.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import './App.css';
+// import 'leaflet/dist/leaflet.css';
+
+// const center = [10.123, 12.233]
+
+// export default function App () {
+//     return (
+//         <MapContainer
+//             center={center}
+//             zoom={12}
+//         >
+
+//         </MapContainer>
+//     )
+// }
