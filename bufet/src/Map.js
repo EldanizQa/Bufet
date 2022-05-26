cb127d4978a66168ddfdd191f04f0ae62768e394
EldanizQa <kewa.miki2@gmail.com>
2022-05-27 02:24:43 +0400

    add markers!

--- bufet/src/Map.js
+++ bufet/src/Map.js
@@ -12,25 +12,48 @@ import './App.css';
 import L from "leaflet"
 // import mapIcon from './assets/Icons'
 
+
+const markerIcon = L.icon({
+    iconUrl: require("./assets/Icons/icon2.png"),
+    iconSize: [35,40]
+})
+
+
 const center = [40.367421050763554, 48.15512358371176];
-
-
 
  function Map() {
   return (
     <MapContainer
       center={center}
-      zoom={12}
+      zoom={8}
       style={{ width: '100vw', height: '100vh' }}
     >
       <TileLayer
         url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=dZOt5h8VzjgQ9hrjLbT7"
         attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
       />
-       <Marker position={center} >
-        <Popup>
-          <a href='https://en.wikipedia.org/wiki/Azerbaijan'>Welcome</a> <br /> 
-        </Popup>
+       <Marker position={[39.27187552341733, 45.40949814987457]} icon={markerIcon} >
+         <Popup>
+          <a href="https://ru.wikipedia.org/wiki/Нахичевань">Naxcivan-столица Нахичеванской Автономной Республики</a> 
+        </Popup> 
+      </Marker>
+
+      <Marker position={[41.37480125307747, 48.539552838440464]} icon={markerIcon} >
+         <Popup>
+          <a href="https://ru.wikipedia.org/wiki/Нахичевань">Quba</a> 
+        </Popup> 
+      </Marker>
+
+      <Marker position={[40.411089277947596, 49.876886131147195]} icon={markerIcon} >
+         <Popup>
+          <a href="https://ru.wikipedia.org/wiki/Нахичевань">Baki</a> 
+        </Popup> 
+      </Marker>
+
+      <Marker position={[39.75329240226435, 46.74566736831197]} icon={markerIcon} >
+         <Popup>
+          <a href="https://ru.wikipedia.org/wiki/Нахичевань">Şuşa</a> 
+        </Popup> 
       </Marker>
       {
         statesData.features.map((state) => {
@@ -38,7 +61,7 @@ const center = [40.367421050763554, 48.15512358371176];
 
           return (<Polygon
             pathOptions={{
-              fillColor: '#008000',
+              fillColor: 'orange',
               fillOpacity: 0.7,
               weight: 2,
               opacity: 1,
@@ -50,7 +73,7 @@ const center = [40.367421050763554, 48.15512358371176];
               mouseover: (e) => {
                 const layer = e.target;
                 layer.setStyle({
-                  fillColor: "black",
+                  fillColor: "green",
                   fillOpacity: 0.7,
                   weight: 2,
                   opacity: 1,
@@ -64,7 +87,7 @@ const center = [40.367421050763554, 48.15512358371176];
                   weight: 2,
                   dashArray: "3",
                   color: 'white',
-                  fillColor: '#008000'
+                  fillColor: 'orange'
                 });
               },
               click: (e) => {
