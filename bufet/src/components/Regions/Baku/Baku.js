01f200a76aac719bae073d9a24932282d3614c6b
Shamistan Aliyev~ <shamistanaliyev237@gmail.com>
2022-06-02 00:07:33 +0400

    final fulling

--- bufet/src/components/Regions/Baku/Baku.js
+++ bufet/src/components/Regions/Baku/Baku.js
@@ -11,6 +11,7 @@ import "leaflet/dist/leaflet.css";
 import { statesData } from "./data";
 import "./Baku.css";
 import L, { marker } from "leaflet";
+import Sliders from "../../Slider/Slider";
 
 const markerIcon = L.icon({
   iconUrl: require("../../../assets/Icons/icon2.png"),
@@ -67,10 +68,10 @@ const Baku = () => {
         "From the 13th to the 16th centuries, the palace of the Shirvanshahs was built. The sights of Azerbaijan include this architectural complex, which includes a Hamer divan courtyard, a mosque and a tomb. It even found a place for a bathhouse and the mausoleum of Bakuvi.",
     },
     {
-      title: " Mausoleum of Momine-Khatun",
+      title: " Heydar Aliyev Center",
       src: require("../../../assets/images/7.jpg"),
       description:
-        "The ancestor of the aldehyde dynasty is Momine-Khatun. It was for her that the Momine-Khatun Mausoleum was built in the 12th century. The author and founder was atabek Jahan Pahlavan. He dedicated the majestic building to his mother, Momine-Khatun. The complexity of the decoration attracts specialists and lovers of ancient architecture from all over the world. The geometric ornament in stone is used as the main decorative technique. On it you can see many quotes from the Koran, presented in the form of beautiful inscriptions.",
+        "The Heydar Aliyev Center is a 57,500 m2 building complex in Baku, Azerbaijan designed by Iraqi-British architect Zaha Hadid and noted for its distinctive architecture and flowing, curved style that eschews sharp angles. The center is named after Heydar Aliyev, the first secretary of Soviet Azerbaijan from 1969 to 1982, and president of Azerbaijan Republic from October 1993 to October 2003.",
     },
     {
       title: " Flame towers",
@@ -84,90 +85,95 @@ const Baku = () => {
     setCurrentMarker(marker);
   };
   return (
-    <div className="map-div">
-      <MapContainer
-        zoomControl={true}
-        center={center}
-        zoom={zoom}
-        scrollWheelZoom={false}
-        style={{
-          width: "100%",
-          height: "100vh",
-          margin: "0 auto",
-        }}
-        className="map-container"
-      >
-        <TileLayer
-          url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=dZOt5h8VzjgQ9hrjLbT7"
-          attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-        />
-        {/* <ZoomControl
+    <>
+      <div className="map-div">
+        <MapContainer
+          zoomControl={true}
+          center={center}
+          zoom={zoom}
+          scrollWheelZoom={false}
+          style={{
+            width: "100%",
+            height: "100vh",
+            margin: "0 auto",
+          }}
+          className="map-container"
+        >
+          <TileLayer
+            url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=dZOt5h8VzjgQ9hrjLbT7"
+            attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+          />
+          {/* <ZoomControl
         zoomControl={false}
         >
         </ZoomControl> */}
 
-        {markers.map((marker) => (
-          <Marker position={marker.positions} icon={markerIcon}>
-            <Popup>
-              <a onClick={() => getMarker(marker)}>{marker.title}</a>
-            </Popup>
-          </Marker>
-        ))}
+          {markers.map((marker) => (
+            <Marker position={marker.positions} icon={markerIcon}>
+              <Popup>
+                <a onClick={() => getMarker(marker)}>{marker.title}</a>
+              </Popup>
+            </Marker>
+          ))}
 
-        {statesData.features.map((state) => {
-          const coordinates = state.geometry.coordinates[0].map((item) => [
-            item[1],
-            item[0],
-          ]);
+          {statesData.features.map((state) => {
+            const coordinates = state.geometry.coordinates[0].map((item) => [
+              item[1],
+              item[0],
+            ]);
 
-          return (
-            <Polygon
-              pathOptions={{
-                fillColor: "rgb(106, 114, 132)",
-                fillOpacity: 0.7,
-                weight: 1,
-                opacity: 1,
-                dashArray: 3,
-                color: "black",
-              }}
-              positions={coordinates}
-              eventHandlers={{
-                mouseover: (e) => {
-                  const layer = e.target;
-                  layer.setStyle({
-                    fillColor: "orange",
-                    fillOpacity: 0.7,
-                    weight: 2,
-                    opacity: 1,
-                    color: "white",
-                  });
-                },
-                mouseout: (e) => {
-                  const layer = e.target;
-                  layer.setStyle({
-                    fillOpacity: 0.7,
-                    weight: 1,
-                    dashArray: "3",
-                    color: "white",
-                    fillColor: "rgb(106, 114, 132)",
-                  });
-                },
-                click: (e) => {},
-              }}
-            />
-          );
-        })}
-      </MapContainer>
-      {currentMarker && (
-        <div>
-          <div>{currentMarker.title}</div>
+            return (
+              <Polygon
+                pathOptions={{
+                  fillColor: "rgb(106, 114, 132)",
+                  fillOpacity: 0.7,
+                  weight: 1,
+                  opacity: 1,
+                  dashArray: 3,
+                  color: "black",
+                }}
+                positions={coordinates}
+                eventHandlers={{
+                  mouseover: (e) => {
+                    const layer = e.target;
+                    layer.setStyle({
+                      fillColor: "orange",
+                      fillOpacity: 0.7,
+                      weight: 2,
+                      opacity: 1,
+                      color: "white",
+                    });
+                  },
+                  mouseout: (e) => {
+                    const layer = e.target;
+                    layer.setStyle({
+                      fillOpacity: 0.7,
+                      weight: 1,
+                      dashArray: "3",
+                      color: "white",
+                      fillColor: "rgb(106, 114, 132)",
+                    });
+                  },
+                  click: (e) => {},
+                }}
+              />
+            );
+          })}
+        </MapContainer>
+        {currentMarker && (
           <div>
-            <img src={currentMarker.src} />
+            <div>{currentMarker.title}</div>
+            <div>
+              <img src={currentMarker.src} />
+            </div>
+            <div>{currentMarker.descriprion}</div>
           </div>
-          <div>{currentMarker.descriprion}</div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+      <div className="sliderAz">
+        <Sliders slider={sliderObj} />
+      </div>
+    </>
   );
 };
 export default Baku;
