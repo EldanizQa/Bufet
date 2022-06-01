fa4ba80fba0e3d9c1dbbc08018b48795fd3aee25
Shamistan Aliyev~ <shamistanaliyev237@gmail.com>
2022-06-01 23:45:15 +0400

    shamistan fulling content

--- bufet/src/components/Regions/Baku/Baku.js
+++ bufet/src/components/Regions/Baku/Baku.js
@@ -11,6 +11,7 @@ import "leaflet/dist/leaflet.css";
 import { statesData } from "./data";
 import "./Baku.css";
 import L from "leaflet";
+import Sliders from "../../Slider/Slider";
 
 const markerIcon = L.icon({
   iconUrl: require("../../../assets/Icons/icon2.png"),
@@ -49,87 +50,118 @@ const markers = [
 const center = [40.40508337134207, 49.86391827210291];
 let zoom = 9;
 const Baku = () => {
+  const sliderObj = [
+    {
+      title: " Maiden tower",
+      src: require("../../../assets/images/1.jpg"),
+      description:
+        "The Maiden Tower tower is rightly called the most mysterious monument in Baku. It is located on the territory of the Icheri Sheher fortress. The uniqueness of the architecture of Gyz Galasy lies in the fact that the tower is located on a ledge of a large rock. A gray lime cylinder was used as a building material. The height of the fortress reaches 28 m, and its diameter is 16.5 m.",
+    },
+    {
+      title: " Palace of the Shirvanshahs",
+      src: require("../../../assets/images/2.jpg"),
+      description:
+        "From the 13th to the 16th centuries, the palace of the Shirvanshahs was built. The sights of Azerbaijan include this architectural complex, which includes a Hamer divan courtyard, a mosque and a tomb. It even found a place for a bathhouse and the mausoleum of Bakuvi.",
+    },
+    {
+      title: " Mausoleum of Momine-Khatun",
+      src: require("../../../assets/images/7.jpg"),
+      description:
+        "The ancestor of the aldehyde dynasty is Momine-Khatun. It was for her that the Momine-Khatun Mausoleum was built in the 12th century. The author and founder was atabek Jahan Pahlavan. He dedicated the majestic building to his mother, Momine-Khatun. The complexity of the decoration attracts specialists and lovers of ancient architecture from all over the world. The geometric ornament in stone is used as the main decorative technique. On it you can see many quotes from the Koran, presented in the form of beautiful inscriptions.",
+    },
+    {
+      title: " Flame towers",
+      src: require("../../../assets/images/4.jpg"),
+      description:
+        "These are three high-rise buildings, including a hotel, apartments and offices. The total building area is 227 thousand m². The construction of the buildings was started in October 2007 by the Azerbaijani-Turkish company DIA Holding. Initially, it was planned to complete the construction of buildings by December 2010, but, according to representatives of the construction company, due to due to adverse weather conditions, the dates were postponed. Construction was only completed in 2012.",
+    },
+  ];
   const [currentMarker, setCurrentMarker] = useState(null);
   const getMarkerId = (marker) => {
     setCurrentMarker(marker);
   };
   return (
-    <div className="map-div">
-      <MapContainer
-        zoomControl={true}
-        center={center}
-        zoom={zoom}
-        scrollWheelZoom={false}
-        style={{
-          width: "100%",
-          height: "100vh",
-          margin: "0 auto",
-        }}
-        className="map-container"
-      >
-        <TileLayer
-          url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=dZOt5h8VzjgQ9hrjLbT7"
-          attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-        />
-        {/* <ZoomControl
+    <>
+      <div className="map-div">
+        <MapContainer
+          zoomControl={true}
+          center={center}
+          zoom={zoom}
+          scrollWheelZoom={false}
+          style={{
+            width: "100%",
+            height: "100vh",
+            margin: "0 auto",
+          }}
+          className="map-container"
+        >
+          <TileLayer
+            url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=dZOt5h8VzjgQ9hrjLbT7"
+            attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+          />
+          {/* <ZoomControl
         zoomControl={false}
         >
         </ZoomControl> */}
 
-        {markers.map((marker) => (
-          <Marker position={marker.positions} icon={markerIcon}>
-            <Popup>
-              <a onClick={() => getMarkerId(marker)}>{marker.title}</a>
-            </Popup>
-          </Marker>
-        ))}
+          {markers.map((marker) => (
+            <Marker position={marker.positions} icon={markerIcon}>
+              <Popup>
+                <a onClick={() => getMarkerId(marker)}>{marker.title}</a>
+              </Popup>
+            </Marker>
+          ))}
 
-        {statesData.features.map((state) => {
-          const coordinates = state.geometry.coordinates[0].map((item) => [
-            item[1],
-            item[0],
-          ]);
+          {statesData.features.map((state) => {
+            const coordinates = state.geometry.coordinates[0].map((item) => [
+              item[1],
+              item[0],
+            ]);
 
-          return (
-            <Polygon
-              pathOptions={{
-                fillColor: "rgb(106, 114, 132)",
-                fillOpacity: 0.7,
-                weight: 1,
-                opacity: 1,
-                dashArray: 3,
-                color: "black",
-              }}
-              positions={coordinates}
-              eventHandlers={{
-                mouseover: (e) => {
-                  const layer = e.target;
-                  layer.setStyle({
-                    fillColor: "orange",
-                    fillOpacity: 0.7,
-                    weight: 2,
-                    opacity: 1,
-                    color: "white",
-                  });
-                },
-                mouseout: (e) => {
-                  const layer = e.target;
-                  layer.setStyle({
-                    fillOpacity: 0.7,
-                    weight: 1,
-                    dashArray: "3",
-                    color: "white",
-                    fillColor: "rgb(106, 114, 132)",
-                  });
-                },
-                click: (e) => {},
-              }}
-            />
-          );
-        })}
-      </MapContainer>
-      {currentMarker && <div>123</div>}
-    </div>
+            return (
+              <Polygon
+                pathOptions={{
+                  fillColor: "rgb(106, 114, 132)",
+                  fillOpacity: 0.7,
+                  weight: 1,
+                  opacity: 1,
+                  dashArray: 3,
+                  color: "black",
+                }}
+                positions={coordinates}
+                eventHandlers={{
+                  mouseover: (e) => {
+                    const layer = e.target;
+                    layer.setStyle({
+                      fillColor: "orange",
+                      fillOpacity: 0.7,
+                      weight: 2,
+                      opacity: 1,
+                      color: "white",
+                    });
+                  },
+                  mouseout: (e) => {
+                    const layer = e.target;
+                    layer.setStyle({
+                      fillOpacity: 0.7,
+                      weight: 1,
+                      dashArray: "3",
+                      color: "white",
+                      fillColor: "rgb(106, 114, 132)",
+                    });
+                  },
+                  click: (e) => {},
+                }}
+              />
+            );
+          })}
+        </MapContainer>
+        {currentMarker && <div>123</div>}
+      </div>
+      <div className="sliderAz">
+        <Sliders slider={sliderObj} />
+      </div>
+    </>
   );
 };
 export default Baku;
--- bufet/src/components/Regions/Quba/Quba.js
+++ bufet/src/components/Regions/Quba/Quba.js
@@ -12,6 +12,7 @@ import { statesData } from "./data";
 import "./Quba.css";
 import L from "leaflet";
 import { Link } from "react-router-dom";
+import Sliders from "../../Slider/Slider";
 
 const markerIcon = L.icon({
   iconUrl: require("../../../assets/Icons/icon2.png"),
@@ -20,110 +21,141 @@ const markerIcon = L.icon({
 const center = [41.191148227358866, 48.49190523563717];
 let zoom = 10;
 const Quba = () => {
+  const sliderObj = [
+    {
+      title: " Friday Mosque",
+      src: require("../../../assets/images/5.jpg"),
+      description:
+        "Juma Mosque (Friday Mosque) of Azerbaijan is one of the main mosques in Quba region of Azerbaijan. The construction of the mosque dates back to to the 19th century. The unique side of the mosque is its structure which is not typical to other places of Azerbaijan except Quba. Its construction is typical only for the mosques of Quba province. The mosque has a cylinder shaped structure and has the form of regular octagon. In fact, the interior of the mosque is bigger than expected from outside. There is a large dome with a diameter of 16 meters covering the hall. Inside there is a big hall crowned with a huge 16 m diameter dome. After Juma Mosque constructed in Quba, the same type of structures started to appear in other regions of Azerbaijan.",
+    },
+    {
+      title: " Khinalig",
+      src: require("../../../assets/images/6.png"),
+      description:
+        "One of Azerbaijan’s top ethno-tourism spots, the picturesque village of Khinalig is located at over 2,000 metres and is said to be connected to the story of Noah, who some believe dropped anchor here during the Great Flood. Due to the steep terrain, the village’s 380 houses – many between 200-300 years old – are built very close together, with each roof doubling up as the courtyard for the house above. As descendants of one of the ancient Caucasian Albanian tribes, the locals speak a completely unique language. Travelling here will allow you to discover an intriguing local culture while experiencing the full majesty of the Caucasus Mountains.",
+    },
+    {
+      title: " Bathhouse where Alexandre Dumas washed",
+      src: require("../../../assets/images/13.jpg"),
+      description:
+        "For the population of Azerbaijan, baths have always been a special place where people rested not only physically, but also spiritually. In particular, on the territory of the city of Guba there is one old bathhouse built in the 18th century. It is built of red brick, and the building is crowned with a large dome, which was designed to maintain the necessary humidity and temperature in the bath.The building is quadrangular. Another name for the bath is Suhur (sometimes Chukhur) hammam. The bath building has 6 rooms, two doors and six windows. The bath was connected to the plumbing. That is, the water from the water supply was supplied from a well located under the bath, or from the city water supply. The bathhouse is interesting because the famous author Alexandre Dumas once bathed here. After that, 150 years later, his great-grandson specially visited the same place. The bath was used for its intended purpose until 1985. However, at present, the bathhouse does not function, but remains only a historical monument.",
+    },
+    {
+      title: " Afurja",
+      src: require("../../../assets/images/14.png"),
+      description:
+        "Afurja is the highest waterfall in Azerbaijan with a height of 75 meters. It has a natural origin and is located in the city of Guba, in particular, near the Tengalty gorge. You can go to it along a narrow rocky road near the rocks. The waterfall is a natural monument and is protected by the state. Local tour guides say that the Afurdzha waterfall was once visited by such famous people as the poet Mikhail Lermontov and the Decembrist Bestuzhev-Marlinsky.",
+    },
+  ];
   return (
-    <div className="map-div">
-      <MapContainer
-        zoomControl={true}
-        center={center}
-        zoom={zoom}
-        scrollWheelZoom={false}
-        style={{
-          width: "100%",
-          height: "100vh",
-          margin: "0 auto",
-        }}
-        className="map-container"
-      >
-        <TileLayer
-          url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=dZOt5h8VzjgQ9hrjLbT7"
-          attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-        />
-        {/* <ZoomControl
+    <>
+      <div className="map-div">
+        <MapContainer
+          zoomControl={true}
+          center={center}
+          zoom={zoom}
+          scrollWheelZoom={false}
+          style={{
+            width: "100%",
+            height: "100vh",
+            margin: "0 auto",
+          }}
+          className="map-container"
+        >
+          <TileLayer
+            url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=dZOt5h8VzjgQ9hrjLbT7"
+            attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+          />
+          {/* <ZoomControl
       zoomControl={false}
       >
       </ZoomControl> */}
-        <Marker
-          position={[39.27187552341733, 45.40949814987457]}
-          icon={markerIcon}
-        >
-          <Popup>
-            <Link href="https://ru.wikipedia.org/wiki/Нахичевань">
-              Naxcivan-столица Нахичеванской Автономной Республики
-            </Link>
-          </Popup>
-        </Marker>
+          <Marker
+            position={[39.27187552341733, 45.40949814987457]}
+            icon={markerIcon}
+          >
+            <Popup>
+              <Link href="https://ru.wikipedia.org/wiki/Нахичевань">
+                Naxcivan-столица Нахичеванской Автономной Республики
+              </Link>
+            </Popup>
+          </Marker>
 
-        <Marker
-          position={[41.37480125307747, 48.539552838440464]}
-          icon={markerIcon}
-        >
-          <Popup>
-            <Link to="/quba">Quba</Link>
-          </Popup>
-        </Marker>
+          <Marker
+            position={[41.37480125307747, 48.539552838440464]}
+            icon={markerIcon}
+          >
+            <Popup>
+              <Link to="/quba">Quba</Link>
+            </Popup>
+          </Marker>
 
-        <Marker
-          position={[40.411089277947596, 49.876886131147195]}
-          icon={markerIcon}
-        >
-          <Popup>
-            <Link href="https://ru.wikipedia.org/wiki/Нахичевань">Baki</Link>
-          </Popup>
-        </Marker>
+          <Marker
+            position={[40.411089277947596, 49.876886131147195]}
+            icon={markerIcon}
+          >
+            <Popup>
+              <Link href="https://ru.wikipedia.org/wiki/Нахичевань">Baki</Link>
+            </Popup>
+          </Marker>
 
-        <Marker
-          position={[39.75329240226435, 46.74566736831197]}
-          icon={markerIcon}
-        >
-          <Popup>
-            <Link to="https://ru.wikipedia.org/wiki/Нахичевань">Şuşa</Link>
-          </Popup>
-        </Marker>
-        {statesData.features.map((state) => {
-          const coordinates = state.geometry.coordinates[0].map((item) => [
-            item[1],
-            item[0],
-          ]);
+          <Marker
+            position={[39.75329240226435, 46.74566736831197]}
+            icon={markerIcon}
+          >
+            <Popup>
+              <Link to="https://ru.wikipedia.org/wiki/Нахичевань">Şuşa</Link>
+            </Popup>
+          </Marker>
+          {statesData.features.map((state) => {
+            const coordinates = state.geometry.coordinates[0].map((item) => [
+              item[1],
+              item[0],
+            ]);
 
-          return (
-            <Polygon
-              pathOptions={{
-                fillColor: "rgb(106, 114, 132)",
-                fillOpacity: 0.7,
-                weight: 1,
-                opacity: 1,
-                dashArray: 3,
-                color: "white",
-              }}
-              positions={coordinates}
-              eventHandlers={{
-                mouseover: (e) => {
-                  const layer = e.target;
-                  layer.setStyle({
-                    fillColor: "orange",
-                    fillOpacity: 0.7,
-                    weight: 2,
-                    opacity: 1,
-                    color: "black",
-                  });
-                },
-                mouseout: (e) => {
-                  const layer = e.target;
-                  layer.setStyle({
-                    fillOpacity: 0.7,
-                    weight: 1,
-                    dashArray: "3",
-                    color: "black",
-                    fillColor: "rgb(106, 114, 132)",
-                  });
-                },
-                click: (e) => {},
-              }}
-            />
-          );
-        })}
-      </MapContainer>
-    </div>
+            return (
+              <Polygon
+                pathOptions={{
+                  fillColor: "rgb(106, 114, 132)",
+                  fillOpacity: 0.7,
+                  weight: 1,
+                  opacity: 1,
+                  dashArray: 3,
+                  color: "white",
+                }}
+                positions={coordinates}
+                eventHandlers={{
+                  mouseover: (e) => {
+                    const layer = e.target;
+                    layer.setStyle({
+                      fillColor: "orange",
+                      fillOpacity: 0.7,
+                      weight: 2,
+                      opacity: 1,
+                      color: "black",
+                    });
+                  },
+                  mouseout: (e) => {
+                    const layer = e.target;
+                    layer.setStyle({
+                      fillOpacity: 0.7,
+                      weight: 1,
+                      dashArray: "3",
+                      color: "black",
+                      fillColor: "rgb(106, 114, 132)",
+                    });
+                  },
+                  click: (e) => {},
+                }}
+              />
+            );
+          })}
+        </MapContainer>
+      </div>
+      <div className="sliderAz">
+        <Sliders slider={sliderObj} />
+      </div>
+    </>
   );
 };
 export default Quba;
