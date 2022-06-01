7c9dce6c98d5bca731fc91addbcbf5fded0bc7d3
Macbook <macbook2020@Macbooks-MacBook-Pro.local>
2022-06-02 01:06:39 +0400

    aqil's changes map method

--- bufet/src/components/Azerbaijan/Azerbaijan.js
+++ bufet/src/components/Azerbaijan/Azerbaijan.js
@@ -12,7 +12,6 @@ const markerIcon = L.icon({
   iconSize: [35, 40],
 });
 const center = [40.367421050763554, 48.15512358371176];
-
 const Azerbaijan = () => {
   const sliderObj = [
     {
@@ -76,6 +75,32 @@ const Azerbaijan = () => {
         "Yukhari Govhar Agha Mosque or Shusha Friday Mosque is the oldest mosque built in the central square of Shusha. The mosque is registered by the Ministry of Culture and Tourism of the Republic of Azerbaijan as a historical and cultural monument of national importance. The construction of the Upper Govhar Agha Mosque was carried out in four stages. The first mosque in the area was built of reeds in about 1750 by the order of Panahali khan, the founder of the Karabakh khanate and Shusha fortress, parallel to the Karabakh khan's palace. After Ibrahimkhalil khan came to power in 1768–1769- A new stone mosque was built on the site of the reed mosque. In the first half of the 19th century, with the financial support of Govhar agha, a third mosque with a double minaret was built on the site of Ibrahimkhalil khan's dilapidated mosque. 1883- The fourth and last Friday mosque of Shusha was built on the site of the third mosque on the basis of Govhar aga's financial resources.",
     },
   ];
+  const markers = [
+    {
+      id: "1",
+      positions: [39.27187552341733, 45.40949814987457],
+      title: "Naxcivan-столица Нахичеванской Автономной Республики",
+      link: "/nakhchivan",
+    },
+    {
+      id: "2",
+      positions: [41.37480125307747, 48.539552838440464],
+      title: "Quba",
+      link: "/quba",
+    },
+    {
+      id: "3",
+      positions: [40.411089277947596, 49.876886131147195],
+      title: "Baku",
+      link: "/baku",
+    },
+    {
+      id: "4",
+      positions: [39.75329240226435, 46.74566736831197],
+      title: "Şuşa",
+      link: "/shusha",
+    },
+  ];
   return (
     <>
       <div className="map-div">
@@ -99,43 +124,15 @@ const Azerbaijan = () => {
       zoomControl={false}
       >
       </ZoomControl> */}
-          <Marker
-            position={[39.27187552341733, 45.40949814987457]}
-            icon={markerIcon}
-          >
-            <Popup>
-              <Link to="/nakhchivan">
-                Naxcivan-столица Нахичеванской Автономной Республики
-              </Link>
-            </Popup>
-          </Marker>
 
-          <Marker
-            position={[41.37480125307747, 48.539552838440464]}
-            icon={markerIcon}
-          >
-            <Popup>
-              <Link to="/quba">Quba</Link>
-            </Popup>
-          </Marker>
+          {markers.map((marker) => (
+            <Marker position={marker.positions} icon={markerIcon}>
+              <Popup>
+                <Link to={marker.link}>{marker.title}</Link>
+              </Popup>
+            </Marker>
+          ))}
 
-          <Marker
-            position={[40.411089277947596, 49.876886131147195]}
-            icon={markerIcon}
-          >
-            <Popup>
-              <Link to="/baku">Baki</Link>
-            </Popup>
-          </Marker>
-
-          <Marker
-            position={[39.75329240226435, 46.74566736831197]}
-            icon={markerIcon}
-          >
-            <Popup>
-              <Link to="/shusha">Şuşa</Link>
-            </Popup>
-          </Marker>
           {statesData.features.map((state) => {
             const coordinates = state.geometry.coordinates[0].map((item) => [
               item[1],
